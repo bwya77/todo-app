c8d867a4ed5f1c53570417409fd4e13c4b051a8e
Bradley Wyatt <brad@thelazyadministrator.com>
2025-03-26 13:34:08 -0500

    removed files that are not needed

--- FileCleanupScript.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Delete duplicate files that cause conflicts
-rm -f "/Users/bradley.wyatt/Git/GitHub/todo-app/todo-app/Presentation/Utilities/SidebarHoverState.swift"
-rm -f "/Users/bradley.wyatt/Git/GitHub/todo-app/todo-app/Presentation/Components/Common/SidebarHoverEnvironment.swift"
-rm -f "/Users/bradley.wyatt/Git/GitHub/todo-app/todo-app/Presentation/Utilities/SidebarViewExtensions.swift"
-
-echo "Cleanup complete!"
